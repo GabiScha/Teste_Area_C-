29cda5e0d0dd5d9f3e5cf8442fd4212997650a9b
Gabriela Schaper <gabrielasch2409@gmail.com>
2024-08-08 17:07:09 -0300

    Add files via upload

---
+++ Form1 (1).cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcule_Click(object sender, EventArgs e)
+        {
+
+            double res = 0;
+            double raio = Double.Parse(txtraio.Text);
+
+
+            res = Math.PI * raio * raio;
+
+            lblresultado.Text = ("O ressultado é : " + res);
+
+
+        }
+    }
+}
